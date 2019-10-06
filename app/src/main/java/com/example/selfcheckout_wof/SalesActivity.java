a820ebeaa8c614edde889166385d767c2681e952
Arturs Elksnis <arturs@elksnis.co.uk>
2019-10-06 16:20:10 +0100

    Fixed a problem where navigation bar was not appearing occasionally on start up.

--- app/src/main/java/com/example/selfcheckout_wof/SalesActivity.java
+++ app/src/main/java/com/example/selfcheckout_wof/SalesActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 
 import com.example.selfcheckout_wof.custom_components.SalesProcessNavigationFragment;
 
@@ -20,27 +21,15 @@ import com.example.selfcheckout_wof.custom_components.SalesProcessNavigationFrag
  */
 public class SalesActivity extends AppCompatActivity
         implements SalesProcessNavigationFragment.OnFragmentInteractionListener {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    private AppCompatActivity thisActivity = null;
 
     /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
     private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
+    private final Handler sysControlsHideHandler = new Handler();
+
+    private View contentView;
     private final Runnable mShowContentRunnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
@@ -50,7 +39,7 @@ public class SalesActivity extends AppCompatActivity
             // Note that some of these constants are new as of API 16 (Jelly Bean)
             // and API 19 (KitKat). It is safe to use them, as they are inlined
             // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+            contentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -58,45 +47,12 @@ public class SalesActivity extends AppCompatActivity
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
     };
-    private View mControlsView;
-    private final Runnable mShowControlsRunnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_sales);
 
         /*
@@ -104,30 +60,7 @@ public class SalesActivity extends AppCompatActivity
          */
         displayAvailableSalesItemsForEdit(0, 0, false);
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.frmSalesItemsListBrowse);
-
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.btnGoToAdmin).setOnTouchListener(mDelayHideTouchListener);
-
-//        findViewById(R.id.btnGoToAdmin).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent showAdminActivity = new Intent(thisActivity, AdminActivity.class);
-//                startActivity(showAdminActivity);
-//            }
-//        });
+        contentView = findViewById(R.id.frmSalesItemsListBrowse);
     }
 
     /**
@@ -139,12 +72,12 @@ public class SalesActivity extends AppCompatActivity
      *                page with items to select.
      */
     private void displayAvailableSalesItemsForEdit(int pageNumber, int parentID, boolean seeMeal){
-        FragmentManager fm = getSupportFragmentManager();
+        final FragmentManager fm = getSupportFragmentManager();
 
-        SalesProcessNavigationFragment navigationFragment =
+        final SalesProcessNavigationFragment navigationFragment =
                 SalesProcessNavigationFragment.newInstance(pageNumber, parentID, true, seeMeal);
 
-        SalesProcessNavigationFragment dataFragment =
+        final SalesProcessNavigationFragment dataFragment =
                 SalesProcessNavigationFragment.newInstance(pageNumber, parentID, false, seeMeal);
 
         fm.beginTransaction()
@@ -165,55 +98,31 @@ public class SalesActivity extends AppCompatActivity
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-
-        thisActivity = this;
+        // Hide the system controls because we want user to use only our interface.
+        hideSystemControls();
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-    private void hide() {
-        // Hide UI first
+    /**
+     * Hides the normal system navigation controls (back, home, show processes)
+     * in a way that's most optimal to the Android versions covered by the app.
+     */
+    private void hideSystemControls() {
+//        sysControlsHideHandler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                ActionBar actionBar = getSupportActionBar();
+//                if (actionBar != null) {
+//                    actionBar.hide();
+//                }
+//
+//                sysControlsHideHandler.postDelayed(mShowContentRunnable, UI_ANIMATION_DELAY);
+//            }
+//        }, 100);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
         }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowControlsRunnable);
-        mHideHandler.postDelayed(mShowContentRunnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mShowContentRunnable);
-        mHideHandler.postDelayed(mShowControlsRunnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in delay milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+        sysControlsHideHandler.postDelayed(mShowContentRunnable, UI_ANIMATION_DELAY);
     }
 
     @Override
